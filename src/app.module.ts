585f86f1d6fac3a570ae0bda7f08445fa4d177f1
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2024-07-27 03:23:18 -0400

    Feature: create CRUD for Status Types. GO

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { StatusTypeModule } from './status-type/status-type.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StatusTypeModule],
   controllers: [AppController],
   providers: [AppService],
 })
