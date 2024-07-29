3c3f70c4f9bf24710719725edbfee49e187b4b68
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2024-07-29 04:32:20 -0400

    Develop: fix prettier problem. GO

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { CharacterController } from './character.controller';
 import { CharacterService } from './character.service';
